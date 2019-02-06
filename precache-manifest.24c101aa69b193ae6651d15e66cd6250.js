2e62b1692d01e1fde7a0132b06658bf492e0206d
Detnex <manjeetit3026@gmail.com>
2019-02-06 23:49:51 +0530

    Updates

---
+++ precache-manifest.24c101aa69b193ae6651d15e66cd6250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "aa53b3d90f1dd0277465",
+    "url": "/robofriends/static/js/main.aa53b3d9.chunk.js"
+  },
+  {
+    "revision": "9b142eae13342eb14328",
+    "url": "/robofriends/static/js/1.9b142eae.chunk.js"
+  },
+  {
+    "revision": "aa53b3d90f1dd0277465",
+    "url": "/robofriends/static/css/main.b08f3f0e.chunk.css"
+  },
+  {
+    "revision": "9b142eae13342eb14328",
+    "url": "/robofriends/static/css/1.8dfc01c0.chunk.css"
+  },
+  {
+    "revision": "effc0ac7f783999479f51bdc91b7c514",
+    "url": "/robofriends/index.html"
+  }
+];
